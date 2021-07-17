f33e88c783a722f0a92f548735a98339cf91363e
rodrigogk87 <rodrigogk87@gmail.com>
2021-07-17 05:44:39 -0300

    simple ethereum web wallet using solidity, webpack and truffle

---
+++ contracts/EtherWallet.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.0;
+
+contract EtherWallet {
+  address payable public owner;
+
+  constructor(address payable _owner) {
+    owner = _owner;
+  }
+
+  function deposit() payable public {
+  }
+
+  function send(address payable to, uint amount) public {
+    if(msg.sender == owner) {
+      require( (address(this).balance > amount),'Not enough founds');
+      to.transfer(amount);
+      return;
+    } 
+    revert('sender is not allowed');
+  }
+
+  function balanceOf() view public returns(uint) {
+    return address(this).balance;
+  }
+}
